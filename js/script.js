4c0e8458b156da6b2bc9bff99f2c052ad47463b9
gegabayte <xasanboymuhammaddinov41@gmail.com>
2024-01-11 23:37:26 +0500

    changed

---
+++ js/script.js
@@ -0,0 +1,28 @@
+// 1-misol
+// let oneNumber = +prompt("ihtiyoriy son kiriting");
+// let result = oneNumber**3;
+// console.log(result);
+
+// 2-misol
+// let firstNumber = +prompt("Ixtiyori son kiriting");
+// const result = firstNumber**3;
+// console.log(result);
+
+//3-misol
+// let firstNumber = +prompt("birinchi sonni kiriting");
+// let secondNumber = +prompt("ikkinchi sonni kiriting");
+// let result = firstNumber + secondNumber;
+// console.log(result);
+
+// 4-misol
+// let n = +prompt("ixtiyoriy ikki honali son kiriting");
+// let b = n%10;
+// let o = n%10;
+// let y = n/100;
+// let res = b + o + y;
+// console.log(res);
+
+// 5-misol
+// let n = +prompt("ihtiyoriy son kiriting");
+// let y = (n/100);
+// console.log(y);
